8d6dffb2d9be5f3a11d1071f6ab7751a2c1f575b
Fábio Lucas Romeiro de Castro <flromeiroc@gmail.com>
2017-07-29 13:02:27 -0300

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,12 @@
+function check(form)
+{
+
+ if(form.userid.value == "matracas" && form.pswrd.value == "incriveis")
+  {
+    window.open('https://time-crash.github.io/')
+  }
+ else
+ {
+   alert("Ta tentando logar sem permissão? Rala daqui e chama alguem do grupo")
+  }
+}
